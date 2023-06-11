32de32d372956eeb4027158cca6324eb65142aeb
Erasmo <erasmoreginogonzalez@gmail.com>
2023-06-10 19:37:24 -0600

    añadiendo todos los archivos a github

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ function encriptar() {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado correctamente";
         parrafo.textContent = "";
-        munheco.src= "img/encriptado.jpg";
+        munheco.src= "img/encriptado.png";
     } else {
         munheco.src= "img/munheco.png";
         alert("Debes ingresar algún texto");
     }
+    document.getElementById("boton-encriptar").style.display = "none";
+    document.getElementById("boton-copiar").style.display = "block";
+
 }
 
 function desencriptar() {
@@ -46,4 +49,44 @@ function desencriptar() {
         munheco.src= "img/text-not-found.png";
         alert("No hay texto para desencriptar");
     }
+    
+    let botonEncriptar = document.getElementById("boton-encriptar");
+    let botonDesencriptar = document.getElementById("boton-desencriptar");
+    let botonCopiar = document.getElementById("boton-copiar");
+
+    if (botonEncriptar.style.display === "none" && botonDesencriptar.style.display === "none") {
+        botonEncriptar.style.display = "block";
+        botonDesencriptar.style.display = "block";
+        botonCopiar.style.display = "none";
+    } else {
+        botonEncriptar.style.display = "none";
+        botonDesencriptar.style.display = "none";
+        botonCopiar.style.display = "block";
+    }
+    
 }
+
+function copiar() {
+    let textoCifrado = document.getElementById("texto").value;
+    navigator.clipboard.writeText(textoCifrado);
+    alert("Texto copiado al portapapeles");
+
+    cambiarBotones();
+
+}
+
+function cambiarBotones() {
+    var botonEncriptar = document.getElementById("boton-encriptar");
+    var botonDesencriptar = document.getElementById("boton-desencriptar");
+    var botonCopiar = document.getElementById("boton-copiar");
+  
+    if (botonEncriptar.style.display === "none") {
+      botonEncriptar.style.display = "block";
+      botonDesencriptar.style.display = "block";
+      botonCopiar.style.display = "none";
+    } else {
+      botonEncriptar.style.display = "none";
+      botonDesencriptar.style.display = "none";
+      botonCopiar.style.display = "block";
+    }
+  }
